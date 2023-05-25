1c896de5e4180e2bfa64bddff68c6a8188f79202
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-05-25 14:56:04 +0100

    new creation on file KeywordBubble (#1)

--- SwiftUIConcept/SwiftUIConceptApp.swift
+++ SwiftUIConcept/SwiftUIConceptApp.swift
@@ -8,32 +8,41 @@
 import SwiftUI
 
 @main
-struct MyApp: App {
+struct SwiftUIConcept : App {
     var body: some Scene {
-        #if os(iOS)
-        MyScene()
-//        WindowGroup {
-//            TabView {
-//                ContentView()
-//                    .tabItem {
-//                        Label("Journal", systemImage: "book")
-//                    }
-//
-//                SettingsView()
-//                    .tabItem {
-//                        Label("Settings", systemImage: "gear")
-//                    }
-//            }
-//        }
-//        #elseif os(macOS)
-//        MyAlternativeScene()
-////        WindowGroup {
-////            AlternativeContentView()
-//////        }
-////
-////        Settings {
-////            SettingsView()
-////        }
-//        #endif
+            WindowGroup {
+                ContentView()
+            }
+        }
+    }
+
+//#if os(iOS)
+//        MyScene()
+        //        WindowGroup {
+        //            TabView {
+        //                ContentView()
+        //                    .tabItem {
+        //                        Label("Journal", systemImage: "book")
+        //                    }
+        //
+        //                SettingsView()
+        //                    .tabItem {
+        //                        Label("Settings", systemImage: "gear")
+        //                    }
+        //            }
+        //        }
+        //        #elseif os(macOS)
+        //        MyAlternativeScene()
+        ////        WindowGroup {
+        ////            AlternativeContentView()
+        //////        }
+        ////
+        ////        Settings {
+        ////            SettingsView()
+        ////        }
+        //        #endif
+        //    }
+        //}
 //    }
+//}
 //}
